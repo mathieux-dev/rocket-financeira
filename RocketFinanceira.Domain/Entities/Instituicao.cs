d9e73d4c615a04dae129d7e03d8b3931972ffe30
Mateus Mourao <mateusvmourao@gmail.com>
2024-10-31 23:33:45 -0300

    feat: alteração para melhor funcionamento da entidade

--- RocketFinanceira.Domain/Entities/Instituicao.cs
+++ RocketFinanceira.Domain/Entities/Instituicao.cs
@@ -7,6 +7,7 @@ public class Instituicao
     public Guid Id { get; set; }
     public required string Nome { get; set; }
     public required string CNPJ { get; set; }
+    public required bool Ativo { get; set; }
     public required Endereco Endereco { get; set; }
     public required Contato Contato { get; set; }
 }
